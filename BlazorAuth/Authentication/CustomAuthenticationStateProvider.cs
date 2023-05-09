8cddc39c34cb0da0ce5079d84ace75713bba9e23
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-05-09 21:22:00 +0300

    Add project files.

---
+++ BlazorAuth/Authentication/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorAuth.Authentication
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var identity = new ClaimsIdentity();
+
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(identity)));
+        }
+    }
+}
---
+++ BlazorAuth/Authentication/RedirectToLogin.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorAuth.Authentication
+{
+    public class RedirectToLogin : ComponentBase
+    {
+        [Inject]
+        private NavigationManager? _navigationManager { get; set;}
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            _navigationManager?.NavigateTo("/login");
+        }
+    }
+}
